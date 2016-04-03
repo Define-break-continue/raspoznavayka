01dfe2b3851ea977b9bb7de8955847eb6d027835
romvano <romvano@gmail.com>
2016-04-04 01:03:21 +0300

    Revert "Revert "merged headers""
    This reverts commit 1d820a05c8f054f15831207e53f03c78b5753aea.

---
+++ aquila-build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src
+# Build directory: /home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(lib)
+SUBDIRS(examples)
+SUBDIRS(tests)
---
+++ aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/tests/Main.cpp" "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/tests/Main.cpp.o"
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/tests/TestAssertHandler.cpp" "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/tests/TestAssertHandler.cpp.o"
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/tests/TestCheckMacros.cpp" "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/tests/TestCheckMacros.cpp.o"
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/tests/TestChecks.cpp" "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/tests/TestChecks.cpp.o"
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/tests/TestCompositeTestReporter.cpp" "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/tests/TestCompositeTestReporter.cpp.o"
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/tests/TestCurrentTest.cpp" "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/tests/TestCurrentTest.cpp.o"
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/tests/TestDeferredTestReporter.cpp" "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/tests/TestDeferredTestReporter.cpp.o"
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/tests/TestExceptions.cpp" "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/tests/TestExceptions.cpp.o"
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/tests/TestMemoryOutStream.cpp" "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/tests/TestMemoryOutStream.cpp.o"
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/tests/TestTest.cpp" "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/tests/TestTest.cpp.o"
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/tests/TestTestList.cpp" "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/tests/TestTestList.cpp.o"
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/tests/TestTestMacros.cpp" "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/tests/TestTestMacros.cpp.o"
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/tests/TestTestResults.cpp" "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/tests/TestTestResults.cpp.o"
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/tests/TestTestRunner.cpp" "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/tests/TestTestRunner.cpp.o"
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/tests/TestTestSuite.cpp" "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/tests/TestTestSuite.cpp.o"
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/tests/TestTimeConstraint.cpp" "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/tests/TestTimeConstraint.cpp.o"
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/tests/TestTimeConstraintMacro.cpp" "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/tests/TestTimeConstraintMacro.cpp.o"
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/tests/TestUnitTestPP.cpp" "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/tests/TestUnitTestPP.cpp.o"
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/tests/TestXmlTestReporter.cpp" "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/TestUnitTest++.dir/tests/TestXmlTestReporter.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/lib/unittestpp/CMakeFiles/UnitTest++.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/lib/unittestpp/."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
