ed1002b438c0fe0156f5c3e9db53d4c5d552333a
romvano <romvano@gmail.com>
2016-04-04 00:59:58 +0300

    Revert "merged headers"
    This reverts commit 46b712390f0d7a45672509c9e5e4b24de81d01d4.

--- src/CRecordedMelody.h
+++ src/CRecordedMelody.h
@@ -1,18 +1,19 @@
 #ifndef __RECORDED_MELODY_
 #define __RECORDED_MELODY_
 
-//#include "CMelody.h"
-//#include "CHash.h"
+#include "CMelody.h"
+#include "CHash.h"
 
 class CRecordedMelody : public CMelody {
     public:
+    CRecordedMelody( std::vector< double > waveform ) : CMelody( waveform ), hash( 0 ) {}
     CRecordedMelody( std::vector< Aquila::SampleType > waveform ) : CMelody( waveform ), hash( 0 ) {}
-    CRecordedMelody( Aquila::WaveFile wavefile ) : CMelody( wavefile ), hash( 0 ) {}
+    CRecordedMelody( Aquila::WaveType waveform ) : CMelody( waveform ), hash( 0 ) {}
     CHash getHash();
 
     protected:
     CHash hash;
-    void setHash( Aquila::WaveFile wavefile );
+    void setHash( Aquila::WaveType waveform );
 };
 
 #endif // __RECORDED_MELODY_
