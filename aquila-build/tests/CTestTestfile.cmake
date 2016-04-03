8ec424edfc9168f8c9d07934490afea18e43a67b
romvano <romvano@gmail.com>
2016-04-04 01:03:22 +0300

    Revert "merged headers"
    This reverts commit 46b712390f0d7a45672509c9e5e4b24de81d01d4.

--- aquila-build/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-src/tests
-# Build directory: /home/ivan/Documents/Raspoznavaika/raspoznavayka/aquila-build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(Aquila_TEST_SUITE "aquila_test")
