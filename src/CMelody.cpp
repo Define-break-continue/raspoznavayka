cec83b66e6413c744a6108e6417f14eb3630d25e
romvano <romvano@gmail.com>
2016-04-14 01:38:22 +0300

    And now the physical magic begins. Added filter A coeffs

--- src/CMelody.cpp
+++ src/CMelody.cpp
@@ -12,6 +12,43 @@ Raspoznavayka::mel_size_t CMelody::getLength() {
     return intervals.size();
 }
 
+// helper functions
+
+// filter A coefficient based on frequency. For the proof-of-concept, a linear interpolation used
+auto dL( Aquila::FrequencyType& f ) {
+    double x1, x2, y1, y2;
+    x1 = x2 = y1 = y2 = 0;
+    Aquila::FrequencyType frequencies[] = {
+            0,    10,  12.5,    16,    20,    25,  31.5,    40,    50,    63,    80,
+                 100,   125,   160,   200,   250,   315,   400,   500,   630,   800,
+                1000,  1250,  1600,  2000,  2500,  3150,  4000,  5000,  6300,  8000,
+               10000, 12500, 16000, 20000
+    };
+    double levels[] = {
+         -300, -70.4, -63.4, -56.7, -50.5, -44.7, -39.4, -34.6, -30.2, -26.2, -22.5,
+               -19.1, -16.1, -13.4, -10.9,  -8.6,  -6.6,  -4.8,  -3.2,  -1.9,  -0.8,
+	           0,   0.6,   1.0,   1.2,   1.3,   1.2,   1.0,   0.5,  -0.1,  -1.1,
+	        -2.5,  -4.3,  -6.6,  -9.3
+    };
+
+    int i = 0;
+    for ( auto frequency : frequencies ) {
+        if ( i > 0 ) {
+            if ( f > frequencies[ i - 1 ] && f <= frequencies[ i ] ) {
+                x1 = frequencies[ i - 1 ];
+		y1 = levels[ i - 1 ];
+		x2 = frequencies[ i ];
+		y2 = levels[ i ];
+	    }
+	}
+    }
+
+    auto k = ( y2 - y1 ) / ( x2 - x1 );
+    auto b = y1 - k * x1;
+    auto dl = k * f + b;
+    return dl;
+}
+
 void CMelody:setIntervals( std::vector< Aquila::SampleType >& ) {
     // TODO main code of physics part will be here
 }
