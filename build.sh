084f965b73823540ef1339dece989e94e8700dd2
muxa <mikael-1994@ya.ru>
2016-04-16 14:43:51 +0300

    architectural crutch

--- build.sh
+++ build.sh
@@ -3,10 +3,33 @@
 # To run program after build, use build.sh -r
 #
 
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -m) tgt=manual;;
+        -r) run=1;;
+    esac
+    shift
+done
+
+tgt="$(gcc -v 2>&1 |grep -e '^Target' |cut -d' ' -f2-)";
+case $tgt in
+    i?86*linux*)
+        cp src/lib/aquila/libAquila.a.86 src/lib/aquila/libAquila.a
+        ;;
+    x86_64*|amd64*)
+        cp src/lib/aquila/libAquila.a.64 src/lib/aquila/libAquila.a
+        ;;
+    "manual") ;;
+    *)
+        echo "Your target architecture is not defined. Copy libAquila.a manually"
+        echo "and issue $0 -m"
+        exit 1
+        ;;
+esac
 rm -rf build/
 mkdir build
 cd build/
 cmake ../src/
 make
 
-[ "x$1" == "x-r" ] && ./raspoznavayka
+[ ! -z ${run} ] && ./raspoznavayka
