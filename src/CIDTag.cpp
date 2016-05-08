c60a1084915796ecd99e2bee9e67f18663fef07e
muxa <mikael-1994@ya.ru>
2016-05-08 14:30:58 +0300

    CIDtag fix

---
+++ src/CIDTag.cpp
@@ -0,0 +1,10 @@
+#include "raspoznavayka.h"
+
+CIDTag::CIDTag( std::string title,
+                std::string artist,
+                std::string album,
+                std::uint8_t year ) : title( title ),
+                                      artist( artist ),
+                                      album( album ),
+                                      year( year ) {}
+
--- src/CIDTag.h
+++ src/CIDTag.h
@@ -1,19 +1,12 @@
-#ifndef __IDTAG_
-#define __IDTAG_
-
-struct CIDTag {
+class CIDTag {
+public:
     std::string title;
     std::string artist;
     std::string album;
     std::uint8_t year;
 
-    CIDTag( std::string title = 0,
-            std::string artist = 0,
-	    std::string album = 0,
-	    std::uint8_t year = 0 ) : title( title ),
-	                              artist( artist ),
-				      album( album ),
-				      year( year ) {}
+    CIDTag( std::string title = "",
+            std::string artist = "",
+            std::string album = "",
+            std::uint8_t year = 0 );
 };
-
-#endif // __IDTAG_
