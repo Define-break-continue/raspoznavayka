c6b4495df5218ec18983e714f9dd41d13410e951
muxa <mikael-1994@ya.ru>
2016-05-10 21:54:52 +0300

    interactive IDTag fill func

--- src/CIDTag.cpp
+++ src/CIDTag.cpp
@@ -3,8 +3,20 @@
 CIDTag::CIDTag( std::string title,
                 std::string artist,
                 std::string album,
-                std::uint8_t year ) : title( title ),
+                std::int16_t year ) : title( title ),
                                       artist( artist ),
                                       album( album ),
                                       year( year ) {}
+
+void CIDTag::interactive_fill() {
+    std::cout << "Tell me of this new song!\nWho's the artist?\n";
+    std::cin >> artist;
+    std::cout << "Awh, I guessed it! And the album is...\n";
+    std::cin >> album;
+    std::cout << "Yeah, it's one of " << artist << "'s best! What's that song's name?\n";
+    std::cin >> title;
+    std::cout << "Names are unneeded. What year " << artist << " recorded it?\n";
+    std::cin >> year;
+    std::cout << "Anyway, I prefer debian_netinst.iso at 44100 Hz to this stuff!\n\n";
+}
 
