3aef93edeb77436f6b5ce4191e549ffa59dec887
muxa <mikael-1994@ya.ru>
2016-05-10 23:09:06 +0300

    CDataBase debug in progress

--- src/raspoznavayka.cpp
+++ src/raspoznavayka.cpp
@@ -21,17 +21,19 @@ int cinputaudio_main() {
 }
 
 int cdatabase_main() {
-    /*
+    
     std::string a = "../resources/test.wav";
     CInputAudio inputAudio = CInputAudio(a);
     auto samples = inputAudio.getSignal();
     CMelody melody( samples );
-    */
+    
+    /*
     std::vector< Raspoznavayka::interval_t > intervals(11);
     //for( Raspoznavayka::interval_t i = Raspoznavayka::dP4_interval; i < Raspoznavayka::A4_interval; i++ )
     for( uint8_t i = -5; i < 6; i++ )
         intervals.push_back( static_cast<Raspoznavayka::interval_t>(i) );
     CMelody melody( intervals );
+    */
 	CIDTag idtag = CIDTag( "title", "artist", "album", 1999 );
     CInDBMelody DBmelody( melody, idtag );
     CDataBase::getInstance().addMelody( DBmelody );
